71f7a1c79a156744a450f38fe81512c81a272b30
Valentin Lemaire <valentinlemaire337@gmail.com>
2023-03-21 11:43:54 +0100

    update read / function IIFE

--- assets/script/listV2.js
+++ assets/script/listV2.js
@@ -16,21 +16,6 @@ form.addEventListener('submit', (even) =>{
     formValidation();
 })
 
-function formValidation(){
-    if((textInput.value ==="") || (categoryInput.value ==="")){
-        console.log('Veuillez renseigner les champs');
-        msg.innerHTML = 'Veuillez renseigner ce champ';
-    }else{
-        console.log('Réussi');
-        msg.innerHTML = '';
-        acceptDatas();
-        add.setAttribute("data-bs-dismiss", "modal");    // la modal disparait après validation du formulaire
-    }
-}
-
-
-
-
 let acceptDatas = () =>{
     datas.push({
         category: categoryInput.value,
@@ -43,6 +28,18 @@ let acceptDatas = () =>{
     read ();
 }
 
+function formValidation(){
+    if((textInput.value ==="") || (categoryInput.value ==="")){
+        console.log('Veuillez renseigner les champs');
+        msg.innerHTML = 'Veuillez renseigner ce champ';
+    }else{
+        console.log('Réussi');
+        msg.innerHTML = '';
+        acceptDatas();
+    }
+    add.setAttribute("data-bs-dismiss", "modal");       // la modal disparait après validation du formulaire
+}
+
 
 
 
@@ -51,22 +48,29 @@ let resetForm = () => {                 // on reset le formulaire après validat
     dateInput.value = "";
     textarea.value = "";
     };
+
 // Fonction read
     let read = () => {
         datas.map((datas, index) => {
             return (tasks.innerHTML += `
-            <div id=${index}>
-            <span class="fw-bold">${datas.category}
-            <span class="fw-bold">${datas.text}</span>
-            <span class="small text-secondary">${datas.date}</span>
-            <span class="small text-secondary">${datas.description}</span>
-            <input type="checkbox" id="pinedCheckbox" onchange="pinedElement(this)"">
-            </div>
-            `)
+                                            <div id=${index}>
+                                            <span class="fw-bold">${datas.category}
+                                            <span class="fw-bold">${datas.text}</span>
+                                            <span class="small text-secondary">${datas.date}</span>
+                                            <span class="small text-secondary">${datas.description}</span>
+                                            <input type="checkbox" id="pinedCheckbox" onchange="pinedElement(this)"">
+                                            <span class="options">
+                                            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                                            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+                                            </span>
+
+                                            </div>
+                                            `
+                    )       
         })
     }
-    (function () {
-        datas = JSON.parse(localStorage.getItem('data'))
+    (() => {
+        datas = JSON.parse(localStorage.getItem('datas') || [])
         console.log(datas);
-        read ()
+        read();
     })();
