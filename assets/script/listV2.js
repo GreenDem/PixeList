00bba0d58e2f516784b95cc08708022594a81076
Nathan <demory.nathan@gmail.com>
2023-03-21 16:10:43 +0100

    UPDATE CLEAR

--- assets/script/listV2.js
+++ assets/script/listV2.js
@@ -58,13 +58,13 @@ let resetForm = () => {                 // on reset le formulaire après validat
         datas.map((datas, index) => {
             return (tasks.innerHTML += `
                                             <div id=${index}>
-                                            <span class="fw-bold">${datas.category}
+                                            <span class="fw-bold">${datas.category}</span>
                                             <span class="fw-bold">${datas.text}</span>
                                             <span class="small text-secondary">${datas.date}</span>
                                             <span class="small text-secondary">${datas.description}</span>
                                             <input type="checkbox" id="pinedCheckbox" onchange="pinedElement(this)"">
                                             <span class="options">
-                                            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                                            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#formModify" class="fas fa-edit"></i>
                                             <i onClick ="deleteTask(this);read()" class="fas fa-trash-alt"></i>
                                             </span>
 
@@ -79,3 +79,48 @@ let resetForm = () => {                 // on reset le formulaire après validat
     read();
 })();
 
+// UPDATE TASK
+
+let modalTarget = document.querySelectorAll('.modify')
+let textInputModify = document.getElementById('textInputModify');
+let dateInputModify = document.getElementById('dateInputModify');
+let textareaModify = document.getElementById('textareaModify');
+let categoryModify = document.getElementById('categoryInputModify');
+
+
+
+
+let editTask = (event) => {
+    let editTargetId =event.parentElement.parentElement.id
+
+    let category =datas[editTargetId].category
+    let date=datas[editTargetId].date
+    let description = datas[editTargetId].description
+    let text = datas[editTargetId].text
+
+    modalTarget[0].value=category
+    modalTarget[1].value=text
+    modalTarget[2].value=date
+    modalTarget[3].value=description   
+
+console.log(editTargetId)
+    document.getElementById('modify').addEventListener('click', (e) =>{
+    console.log('On a pas tous la chance de faire du ski en été')
+    datas.splice(editTargetId, 1, ({
+        category: categoryModify.value,
+        text : textInputModify.value,
+        date : dateInputModify.value,
+        description : textareaModify.value,
+    }))
+    localStorage.setItem("data", JSON.stringify(datas))
+    resetFormModify()
+    read()
+})
+}
+
+let resetFormModify = () => {                 // on reset le formulaire après validation
+    categoryModify.value= "";
+    textInputModify.value = "";
+    dateInputModify.value = "";
+    textareaModify.value = "";
+    };
