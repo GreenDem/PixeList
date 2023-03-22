b5cfdb6e5cd30057f1c8d41d5f73a83a9afd7dc0
Alex80100 <flamentalexandre20@gmail.com>
2023-03-22 09:43:17 +0100

    bar exp

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,37 @@
-//START
+// JS Barre d'xp
+
+let maxBar = 100; // quantite max de la barre
+let currentBar = 0; // initialisation à zéro
+let progressBar; // Objet JS barre de progression
+let intervalId; // 
+
+// Fonction d'initalisation de la barre 
+let initialisation = function (){
+    progressBar = document.getElementById('progressBar');
+    progressBar.value = currentBar;
+    progressBar.max = maxBar;
+}
+
+// Itération à l'intérieur de la barre de progression 
+let displayBar = function (){
+    currentBar++;
+    progressBar.value = currentBar;
+}
+
+// Appel de display barre toute les 100ms
+// intervalId = setInterval(displayBar, 100)
+
+
+// Remise a zero de la barre qd maxBar est atteint
+displayBar = function() {
+    currentBar++;
+    if (currentBar > maxBar) {
+        clearInterval(intervalId);
+    } else{ 
+    progressBar.value = currentBar;
+}}
+
+window.onload = function() {
+    initialisation();
+    intervalId = setInterval(displayBar , 100);
+}
